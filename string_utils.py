8fa2c63bbcdca6b94211e40fdc7125e8a55a2ed1
rand-17 <mohamedrand3@gmail.com>
2025-11-20 00:50:10 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,33 +1,37 @@
 def split_before_each_uppercases(formula):
-  start = 0
-  split_formula = []
+    start = 0
+    split_formula = []
 
- if formula == "":
-     return split_formula
+    if formula == "":
+      return split_formula
+      
 
- for end in range(1,len(formula)):
-     if formula[end].isupper():
+    
+    for end in range(1,len(formula)):
+        if formula[end].isupper():
+            split_formula.append(formula[start:end])
+            start = end
+        
+   
 
-split_formula.append(formula[start:end])
-  start = end
+    split_formula.append(formula[start:])
 
- split_formula.append(formula[start:])
- return split_formula
+    return split_formula
 
 
 def split_at_first_digit(formula):
   start = 1
-  endlist=[]
+  endlist = []
 
   for digitornum in formula[1:]:
     if digitornum.isdigit():
       break
     start += 1
 
-    if start == len(formula):
-        return formula, 1
+  if start == len(formula):
+    return formula, 1
 
-    letters = formula[:start]
-    num = int(formula[:start])
+  letters = formula[:start]
+  nums = int(formula[start:])
 
-    return letters, num
+  return letters, nums
